5bac160d3c0c154fd69a267b9377150de0bf3ed1
youu129 <youunesaireneel@gmail.com>
2026-07-23 02:15:06 +0530

    membermodel2

--- App/models.py
+++ App/models.py
@@ -1,14 +1,17 @@
 from django.db import models
-
+from App.models import MembershipPlan
 # Create your models here.
 class Member(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=15)
+    phone = models.CharField(max_length=15, unique=True)
     age = models.IntegerField()
     gender = models.CharField(max_length=10)
 
-    membership_plan = models.CharField(max_length=50)
+    membership_plan = models.ForeignKey(
+    MembershipPlan,
+    on_delete=models.CASCADE
+    )
 
     joining_date = models.DateField()
 
