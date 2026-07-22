255cc73e4ad03e9718bceeabf8a835cbcca30298
Prajwal-5214 <prajwalraskar123@gmail.com>
2026-07-22 15:06:19 +0530

    new login page

--- App/views.py
+++ App/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def home(request):
     return render(request, 'index.html')
 
 
+
 def member_login(request):
     return render(request, 'login.html')
+
