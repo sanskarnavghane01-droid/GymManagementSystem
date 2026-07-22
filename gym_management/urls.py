0b95e0c78788c3f3dc85b153ddaec4c8a3ac99be
Prajwal-5214 <prajwalraskar123@gmail.com>
2026-07-22 17:04:02 +0530

    easier
    made login page workable by entering member id pass you will be redirected to mmber and same for admin and trainer

--- gym_management/urls.py
+++ gym_management/urls.py
@@ -22,5 +22,8 @@ from App import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('login', views.member_login, name='member_login'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('trainer-dashboard/', views.trainer_dashboard, name='trainer_dashboard'),
+    path('member-dashboard/', views.member_dashboard, name='member_dashboard'),
 ]
