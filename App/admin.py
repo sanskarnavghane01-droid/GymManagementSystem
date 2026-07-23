f37104a9a4a1fbba9d2a17fb92c3397b425295df
Prajwal-5214 <prajwalraskar123@gmail.com>
2026-07-23 19:10:58 +0530

    change

--- App/admin.py
+++ App/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Member
+from .models import MembershipPlan, Member, Payment
 # Register your models here.
+admin.site.register(MembershipPlan)
 admin.site.register(Member)
+admin.site.register(Payment)
--- App/models.py
+++ App/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-from App.models import MembershipPlan
-# Create your models here.
+
+
+class MembershipPlan(models.Model):
+    plan_name = models.CharField(max_length=50)
+    duration_days = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.plan_name
+
+
 class Member(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
@@ -9,24 +18,75 @@ class Member(models.Model):
     gender = models.CharField(max_length=10)
 
     membership_plan = models.ForeignKey(
-    MembershipPlan,
-    on_delete=models.CASCADE
+        MembershipPlan,
+        on_delete=models.CASCADE
     )
 
     joining_date = models.DateField()
-
     expiry_date = models.DateField()
 
     STATUS = [
-        ('Active', 'Active'),
-        ('Expired', 'Expired'),
+        ("Active", "Active"),
+        ("Expired", "Expired"),
     ]
 
     membership_status = models.CharField(
         max_length=10,
         choices=STATUS,
-        default='Active'
+        default="Active"
     )
 
     def __str__(self):
         return self.name
+
+
+class Payment(models.Model):
+    PAYMENT_METHODS = [
+        ("Cash", "Cash"),
+        ("UPI", "UPI"),
+        ("Card", "Card"),
+        ("Net Banking", "Net Banking"),
+    ]
+
+    PAYMENT_STATUSES = [
+        ("Paid", "Paid"),
+        ("Pending", "Pending"),
+        ("Failed", "Failed"),
+    ]
+
+    member = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE
+    )
+
+    membership_plan = models.ForeignKey(
+        MembershipPlan,
+        on_delete=models.CASCADE
+    )
+
+    amount = models.DecimalField(
+        max_digits=8,
+        decimal_places=2
+    )
+
+    payment_date = models.DateField()
+
+    payment_method = models.CharField(
+        max_length=20,
+        choices=PAYMENT_METHODS
+    )
+
+    payment_status = models.CharField(
+        max_length=10,
+        choices=PAYMENT_STATUSES,
+        default='Paid'
+    )
+
+    transaction_id = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return f"{self.member.name} - {self.amount}"
